527b9065b636e044ea71dbb8b5122f976127def7
giacomotolari <tolarigiacomo@gmail.com>
2022-11-28 22:46:59 +0100

    update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,11 @@
-const Post = ({ title }) => {
+import { memo } from "react";
+const Post = memo(({ title }) => {
   console.log(title);
   return (
     <div>
       <h2>{title}</h2>
     </div>
   );
-};
+});
 
 export default Post;
