08563ee07d3146a3d94f6346e170c66e36b6254e
giacomotolari <tolarigiacomo@gmail.com>
2022-12-03 18:55:04 +0100

    create solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
+import Post from "./components/Post";
 import "./styles/App.scss";
 
 function App() {
+  const article1 =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetu ea quibusdam minus placeat consequuntur velit dicta, reprehenderit fuga iste blanditiis, consequatur molestias eos explicabo, praesentiumveritatis officia voluptate doloremque cupiditate! Culpa accusamusnecessitatibus asperiores iusto. Illum, eum soluta. Porro, essedolores, voluptatum modi omnis dicta minus ab consectetur totam assumenda ipsam. At expedita, ducimus veniam harum eum in sapiente.Laborum.";
+  const article2 =
+    " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum aliquid sapiente accusantium id et veniam, sequi explicabo quas, consequatur assumenda tenetur deleniti excepturi maxime fugiat  adipisci odit error amet in. ";
+
+  const article3 =
+    " Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fugit amet odit excepturi esse corporis inventore id aperiam ducimus consequuntur. Iste perferendis nemo, repudiandae id cum hic magni accusamus ipsa! Qui maiores perspiciatis quisquam explicabo nostrum, pariatur eveniet, tenetur sed placeat unde, exercitationem culpa hic distinctio obcaecati ad corrupti nam mollitia rerum ab! Tempora nesciunt odit quos neque ducimus delectus.";
+
   return (
     <div className="App">
       <h1>The Bad Blog</h1>
-      <section>
-        <h2>Post 1</h2>
-        <article>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-          ea quibusdam minus placeat consequuntur velit dicta, reprehenderit
-          fuga iste blanditiis, consequatur molestias eos explicabo, praesentium
-          veritatis officia voluptate doloremque cupiditate! Culpa accusamus
-          necessitatibus asperiores iusto. Illum, eum soluta. Porro, esse
-          dolores, voluptatum modi omnis dicta minus ab consectetur totam
-          assumenda ipsam. At expedita, ducimus veniam harum eum in sapiente.
-          Laborum.
-        </article>
+      <Post title="Post 1" articleText={article1}>
         <br />
         <hr />
         <hr />
@@ -23,39 +21,24 @@ function App() {
           <small>03.12.2022</small>
           <p>Mr.Hohnas</p>
         </div>
-      </section>
-      <section>
-        <h2>Post 2</h2>
-        <article>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
-          aliquid sapiente accusantium id et veniam, sequi explicabo quas,
-          consequatur assumenda tenetur deleniti excepturi maxime fugiat
-          adipisci odit error amet in.
-        </article>
+      </Post>
+
+      <Post title="Post 2" articleText={article2}>
         <br />
         <div>
           <small>03.12.2022</small>
           <p>Mr.Smith</p>
         </div>
-      </section>
-      <section>
-        <h2>Post 3</h2>
-        <article>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          fugit amet odit excepturi esse corporis inventore id aperiam ducimus
-          consequuntur. Iste perferendis nemo, repudiandae id cum hic magni
-          accusamus ipsa! Qui maiores perspiciatis quisquam explicabo nostrum,
-          pariatur eveniet, tenetur sed placeat unde, exercitationem culpa hic
-          distinctio obcaecati ad corrupti nam mollitia rerum ab! Tempora
-          nesciunt odit quos neque ducimus delectus.
-        </article>
+      </Post>
+
+      <Post title="Post 3" articleText={article3}>
         <hr />
         <br />
         <div>
           <small>02.12.2022</small>
           <p>Mr.James</p>
         </div>
-      </section>
+      </Post>
     </div>
   );
 }
