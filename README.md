4b5802df0c42ef8cdcdcba43134e10cd3cde2be7
giacomotolari <tolarigiacomo@gmail.com>
2022-12-04 20:48:26 +0100

    create exercise

--- README.md
+++ README.md
@@ -1,42 +1,9 @@
-# React-optimizing-performance
-
-### What is performance in an app?
-
-Performance has to do with how fast or slows your app loads if the app crashes when it reaches a peak in user activity, and how smooth certain features (like payments) work. Think App says: “Performance is a product of multiple factors: the server, mobile device, network, and programming of the app itself.”
-
-### How important is app performance?
-
-Most (70%) of mobile app users will abandon an app if it takes too long to load. A one-second delay in response can result in a 7% loss in conversion, and nearly one in every two apps are uninstalled within the first month of download.
-
-## How improve performance in React?
-
-There are many factors https://www.codementor.io/blog/react-optimization-5wiwjnf9hj, among the most important we have:
-
-- Use React.Fragments to Avoid Additional HTML Element Wrappers
-- Avoid using Index as Key for map
-- Optimizing the components with React Memo
-- Optimizing the functions components with useCallback and useMemo
-- Use lazy load component
-- Use lazy load image
-- Delete unused code and imports
+# React-optimizing folder-structure
 
 ## what we will learn?
 
-Impove the performance by using React.memo.
-
-## What's going on?
-
-Why have a Blog-Page with three different posts, each post is a Post with different props-values. Each Post component has a console.log that print the props-title. If the console.log will prints in the browser-console that means that the component re render. If you click the ***mode button*** end look the console in the browser, you can see that each Post component and the App component ***re render every time***.
-
-## Why?
-
-In React if a component updates his states, his childs and it self will be re render.
+Impove ordner structure.
 
 ## Task
 
-Solve the problem!
-
-
-## React.memo 
-
-https://dmitripavlutin.com/use-react-memo-wisely/
+Work in group and improve togheter this project-folder-structure
--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,7 @@
-import Post from "./components/Post";
-import "./styles/App.scss";
-import { useState, memo } from "react";
-
-const MemoPost = memo(Post);
+import "./App.scss";
 
 function App() {
-  const [isdarkMode, setIsDarkMode] = useState(false);
-
-  // const setMode = () => setIsDarkMode(isdarkMode ? false : true);
-  const setMode = () => setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode);
-  console.log("app-render");
-
-  const article1 =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetu ea quibusdam minus placeat consequuntur velit dicta, reprehenderit fuga iste blanditiis, consequatur molestias eos explicabo, praesentiumveritatis officia voluptate doloremque cupiditate! Culpa accusamusnecessitatibus asperiores iusto. Illum, eum soluta. Porro, essedolores, voluptatum modi omnis dicta minus ab consectetur totam assumenda ipsam. At expedita, ducimus veniam harum eum in sapiente.Laborum.";
-  const article2 =
-    " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum aliquid sapiente accusantium id et veniam, sequi explicabo quas, consequatur assumenda tenetur deleniti excepturi maxime fugiat  adipisci odit error amet in. ";
-
-  const article3 =
-    " Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fugit amet odit excepturi esse corporis inventore id aperiam ducimus consequuntur. Iste perferendis nemo, repudiandae id cum hic magni accusamus ipsa! Qui maiores perspiciatis quisquam explicabo nostrum, pariatur eveniet, tenetur sed placeat unde, exercitationem culpa hic distinctio obcaecati ad corrupti nam mollitia rerum ab! Tempora nesciunt odit quos neque ducimus delectus.";
-
-  return (
-    <div className={`App ${isdarkMode && "darkMode"}`}>
-      <h1>The Bad Blog</h1>
-      <button onClick={setMode}>
-        {isdarkMode ? "Light Mode" : "Dark Mode"}
-      </button>
-      <Post title="Post 1" articleText={article1}>
-        <br />
-        <hr />
-        <hr />
-        <div>
-          <small>03.12.2022</small>
-          <p>Mr.Hohnas</p>
-        </div>
-      </Post>
-
-      <Post title="Post 2" articleText={article2}>
-        <br />
-        <div>
-          <small>03.12.2022</small>
-          <p>Mr.Smith</p>
-        </div>
-      </Post>
-
-      <MemoPost title="Title 3" articleText={article3} />
-      {/* change title-state to test React.memo */}
-      {/* <Post
-        title={`Post 3 ${isdarkMode ? "Light Mode" : "Dark Mode"}`}
-        articleText={article3}
-      /> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
