3832826e3a2e3e7a5cda2059fe50ab617b889b0e
giacomotolari <tolarigiacomo@gmail.com>
2023-01-11 11:50:44 +0100

    create basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useEffect, useState } from "react";
+
 // import measureDuration from "./util/measureDuration";
 
-function App() {
-  const [comments, setComments] = useState([]);
-  console.log("🚀 ~ file: App.jsx:7 ~ App ~ comments", comments);
+const List = ({ infos }) => {
+  return (
+    <ul>
+      {infos.map((info) => (
+        <li key={crypto.randomUUID()}>{info}</li>
+      ))}
+    </ul>
+  );
+};
 
+const App = () => {
+  const [commentsInfos, setCommentsInfos] = useState({});
+  console.log("🚀 ~ file: App.jsx:7 ~ App ~ commentsInfos", commentsInfos);
+  const API_URL = "https://jsonplaceholder.typicode.com/comments";
+  const commentInfosKeys = Object.keys(commentsInfos);
+  const isCommentInfosEmpty = commentInfosKeys.length === 0;
   // console.log(
   //   measureDuration(function () {
   //     let num = 0;
@@ -16,23 +29,45 @@ function App() {
   //   })
   // );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await fetch(
-          "https://jsonplaceholder.typicode.com/comments"
-        );
-        const data = await result.json();
+  const getComments = async () => {
+    try {
+      const result = await fetch(API_URL);
+      const data = await result.json();
+      return data;
+    } catch (error) {
+      console.log("error", error);
+      return [];
+    }
+  };
 
-        setComments(data);
-      } catch (error) {
-        console.log("error", error);
-      }
+  const getCommentsInfos = async () => {
+    const comments = await getComments();
+    if (comments.length === 0) {
+      return {};
+    }
+    const names = comments.map((comment) => comment.name);
+    const emails = comments.map((comment) => comment.email);
+    const bodies = comments.map((comment) => comment.body);
+    const _commentInfos = {
+      names,
+      emails,
+      bodies,
     };
-    fetchData();
-  }, []);
+    setCommentsInfos(_commentInfos);
+  };
 
-  return <div></div>;
-}
+  return (
+    <div>
+      <button onClick={getCommentsInfos}>Get Comments Infos</button>
+      {!isCommentInfosEmpty &&
+        commentInfosKeys.map((commentInfosKey) => (
+          <section key={crypto.randomUUID()}>
+            <h2>{commentInfosKey}</h2>
+            <List infos={commentsInfos[commentInfosKey]} />
+          </section>
+        ))}
+    </div>
+  );
+};
 
 export default App;
