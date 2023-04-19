a0ddd84a6053ee52222abd195c28c4c29b7a20f9
giacomotolari <tolarigiacomo@gmail.com>
2023-04-19 12:10:31 +0200

    restore app code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
+import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import { MoonLoader } from "react-spinners";
 import "./App.scss";
-import Layout from "./Layout/Layout";
 import routes from "./features/navigation/routes";
+import Layout from "./Layout/Layout";
 
 function App() {
+  const override = {
+    margin: "10% auto",
+    borderColor: "red",
+  };
+
   return (
     <Layout>
-      <main>
-        <Routes>
-          {routes.map((route) => {
-            return <Route key={route.path} {...route} />;
-          })}
-        </Routes>
-      </main>
+      <Suspense
+        fallback={
+          <MoonLoader
+            color="red"
+            cssOverride={override}
+            size={130}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        }
+      >
+        <main>
+          <Routes>
+            {routes.map((route) => {
+              return <Route key={route.path} {...route} />;
+            })}
+          </Routes>
+        </main>
+      </Suspense>
     </Layout>
   );
 }
